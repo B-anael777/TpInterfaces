910b1698ac4b79154c4073b2c2053d829d588c75
PAPAours <anael.bleurvacq777@gmail.com>
2026-09-24 09:59:06 +0200

    feat: completion exercice 5

--- Program.cs
+++ Program.cs
@@ -2,11 +2,14 @@
 var client = new Client("Alice", "alice@example.com");
 var commande = new Commande(101, 150.00m);
 
-AfficherElement(produit);
-AfficherElement(client);
-AfficherElement(commande);
+List<IAffichable> elements = new List<IAffichable>
+{
+    produit,
+    client,
+    commande
+};
 
-static void AfficherElement(IAffichable element)
+foreach (var element in elements)
 {
     element.Afficher();
 }
