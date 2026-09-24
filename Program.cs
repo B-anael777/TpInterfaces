ac75b7505a1a5779aa95aae2e26a87f4b4ebe783
PAPAours <anael.bleurvacq777@gmail.com>
2026-09-24 09:54:32 +0200

    feat: complétion des exercices 1, 2 et 3 avec README

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// Utilisation du type IAffichable avec un Produit
+IAffichable element = new Produit("Clavier", 49.90m);
+element.Afficher();
+
+// La même variable référence maintenant un Client
+element = new Client("Alice", "alice@example.com");
+element.Afficher();
